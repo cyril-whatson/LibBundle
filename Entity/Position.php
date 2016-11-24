9b73a5c3ef3b26e16e5024ce98c78d132681676d
JF <jf@whatson-web.com>
2016-11-24 11:08:56 +0100

    gestion ordre

---
+++ Entity/Position.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace WH\LibBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Class Position
+ *
+ * @package WH\LibBundle\Entity
+ */
+trait Position
+{
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="position", type="integer")
+	 */
+	protected $position;
+
+	/**
+	 * Set position
+	 *
+	 * @param integer $position
+	 *
+	 * @return $this
+	 */
+	public function setPosition($position)
+	{
+		$this->position = $position;
+
+		return $this;
+	}
+
+	/**
+	 * Get position
+	 *
+	 * @return integer
+	 */
+	public function getPosition()
+	{
+		return $this->position;
+	}
+}
