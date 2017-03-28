25323e42d5daa94847e5a49472f06c5d8cbeed37
JF <jf@whatson-web.com>
2017-03-28 08:58:53 +0200

    upgrade SF3

--- Entity/Status.php
+++ Entity/Status.php
@@ -11,83 +11,78 @@ use Gedmo\Mapping\Annotation as Gedmo;
  */
 trait Status
 {
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatusText()
+    {
+        $statuses = self::$statuses;
+        if (!empty($statuses[$this->status])) {
 
-	static $STATUS_DRAFT = 0;
-	static $STATUS_PUBLISHED = 1;
+            return $statuses[$this->status];
+        }
 
-	/**
-	 * Status constructor.
-	 */
-	public function __construct()
-	{
-		$this->status = self::$STATUS_DRAFT;
-	}
+        return '';
+    }
 
-	/**
-	 * @var array
-	 */
-	static protected $statuses = array(
-		0 => 'Draft',
-		1 => 'Published',
-	);
+    static $STATUS_DRAFT = 0;
+    static $STATUS_PUBLISHED = 1;
 
-	/**
-	 * @return array
-	 */
-	static public function getStatuses()
-	{
+    /**
+     * @var array
+     */
+    static protected $statuses = array(
+        0 => 'Draft',
+        1 => 'Published',
+    );
 
-		return self::$statuses;
-	}
+    /**
+     * @return array
+     */
+    static public function getStatuses()
+    {
+        return array_flip(self::$statuses);
+    }
 
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(name="status", type="integer")
-	 */
-	protected $status;
+    /**
+     * Status constructor.
+     */
+    public function __construct()
+    {
+        $this->status = self::$STATUS_DRAFT;
+    }
 
-	/**
-	 * Set status
-	 *
-	 * @param string $status
-	 *
-	 * @return $this
-	 */
-	public function setStatus($status)
-	{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    protected $status;
 
-		$this->status = $status;
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get status
-	 *
-	 * @return string
-	 */
-	public function getStatus()
-	{
-
-		return $this->status;
-	}
-
-	/**
-	 * Get status
-	 *
-	 * @return string
-	 */
-	public function getStatusText()
-	{
-
-		$statuses = self::$statuses;
-		if (!empty($statuses[$this->status])) {
-
-			return $statuses[$this->status];
-		}
-
-		return '';
-	}
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
 
 }
